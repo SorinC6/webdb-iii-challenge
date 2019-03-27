358db505e6fe387f1a6e096cca67b43a3d926ed7
Sorin Chis <kish.sorin@yahoo.com>
2019-03-27 14:59:25 +0200

    added endpoint request for getting all cohorts from database

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+const knexConfig = require('./knexfile').development;
+
+const db = knex(knexConfig);
 
 const server = express();
 server.use(helmet());
 server.use(express.json());
+
+server.get('/api/cohorts', async (req, res) => {
+	try {
+		const cohorts = await db('cohorts');
+		res.status(200).json(cohorts);
+	} catch (error) {
+		res.status(500).json({ error: 'Unable to perform this operation' });
+	}
+});
 
 const port = process.env.PORT || 4000;
 
