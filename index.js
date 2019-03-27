211ee4f7c5f5899dd82d789e9df3c1eed7e13132
Sorin Chis <kish.sorin@yahoo.com>
2019-03-27 14:11:13 +0200

    added knexfile.js and scripts for migrate and seed

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const helmet = require('helmet');
 
 const server = express();
+server.use(helmet());
+server.use(express.json());
 
 const port = process.env.PORT || 4000;
+
+server.get('/', (req, res) => {
+	res.send('<h1>HEllo from the Server</h1>');
+});
 
 server.listen(port, () => console.log(`\n*** Server running on PORT: ${port} ***\n`));
