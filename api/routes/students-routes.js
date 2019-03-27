cd0e408915a383dc4bd05946263385cf4c2f1126
Sorin Chis <kish.sorin@yahoo.com>
2019-03-27 16:20:54 +0200

    added endpoint for saving a student in database

--- api/routes/students-routes.js
+++ api/routes/students-routes.js
@@ -28,4 +28,20 @@ studentsRoute.get('/:id', async (req, res) => {
 	}
 });
 
+studentsRoute.post('/', async (req, res) => {
+	const body = req.body;
+	if (body.name && body.cohort_id) {
+		try {
+			const [ result ] = await db('students').insert(req.body);
+			const student = await db('students').where({ id: result }).first();
+
+			res.status(201).json(student);
+		} catch (error) {
+			res.status(500).json({ error: 'error trying to save the data ' });
+		}
+	} else {
+		res.status(400).json({ message: 'please specified a name and a cohort id for the student' });
+	}
+});
+
 module.exports = studentsRoute;
